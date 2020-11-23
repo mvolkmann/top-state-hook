07fb627362e486fa71596c715ec0586453e648a1
R. Mark Volkmann <r.mark.volkmann@gmail.com>
2020-11-23 13:34:24 -0600

    fixed bug in new getValue function

--- src/top-state-hook.js
+++ src/top-state-hook.js
@@ -10,7 +10,7 @@ const stateMap = {};
 const PREFIX = 'TSH:';
 
 export function getState(name) {
-  return stateMap[name];
+  return stateMap[name].value;
 }
 
 const log = (method, name, text, input, value) => {
